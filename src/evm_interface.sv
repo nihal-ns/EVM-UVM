c175003827b08857d8db19589ef86b6f91c993ce
OmkarBhat08 <omkarbhat@feserver.mirafra.com>
2025-10-28 00:02:41 +0530

    Fixed Interface

--- src/evm_interface.sv
+++ src/evm_interface.sv
@@ -1,4 +1,4 @@
-interface evm_interface(input clk,rst);
+interface evm_interface( clk,rst);
 	logic clk;
 	logic rst;
 	logic vote_candidate_1;
@@ -17,33 +17,33 @@ interface evm_interface(input clk,rst);
 	logic voting_done;
 
 	clocking drv_cb @(posedge clk);
-		output rst,
-		output vote_candidate_1,
-		output vote_candidate_2,
-		output vote_candidate_3,
-		output switch_on_evm,         
-		output candidate_ready,
-		output voting_session_done,
-		output display_results,
-		output display_winner,
+		 output rst,
+		 vote_candidate_1,
+		 vote_candidate_2,
+		 vote_candidate_3,
+		 switch_on_evm,         
+		 candidate_ready,
+		 voting_session_done,
+		 display_results,
+		 display_winner;
 	endclocking
 
 	clocking mon_cb @(posedge clk);
-		input rst,
-		input vote_candidate_1,
-		input vote_candidate_2,
-		input vote_candidate_3,
-		input switch_on_evm,         
-		input candidate_ready,
-		input voting_session_done,
-		input display_results,
-		input display_winner,
+		 input rst,
+		 vote_candidate_1,
+		 vote_candidate_2,
+		 vote_candidate_3,
+		 switch_on_evm,         
+		 candidate_ready,
+		 voting_session_done,
+		 display_results,
+		 display_winner,
 
-		input candidate_name;
-		input invalid_results;
-		input results;
-		input voting_in_progress;
-		input voting_done;
+		 candidate_name,
+		 invalid_results,
+		 results,
+		 voting_in_progress,
+		 voting_done;
 	endclocking
 	
 	modport drv_mod(clocking drv_cb);
