bd4841f0a817d76598a7a1546439a70b3244b496
nihal-ns <nihalns2002@gmail.com>
2025-10-27 20:20:21 +0530

    updated the top file

--- src/top.sv
+++ src/top.sv
@@ -1,10 +1,12 @@
+`define WIDTH 8
 `include "uvm_macros.svh"
-/* `include "interface.sv" */
-/* `include "apbtop.v" */
-/* `include "apb_assertion.sv" */
+`include "evm_interface.sv"
+/* `include "design.v" */
+`include "evm_assertion.sv"
+
 module top;
   import uvm_pkg::*;
-  /* import apb_pkg::*; */
+  import evm_pkg::*;
 
   bit clk;
   bit rst;
@@ -16,19 +18,35 @@ module top;
 
   evm_interface intf(clk, rst);
 
-  Desing DUT ( 
+  Design DUT (
+		.clk(clk),
+		.rst(rst),
+		.vote_candidate_1(intf.vote_candidate_1),
+		.vote_candidate_2(intf.vote_candidate_2),
+		.vote_candidate_3(intf.vote_candidate_3),
+		.switch_on_evm(intf.switch_on_evm),
+		.candidate_ready(intf.candidate_ready),
+		.voting_session_done(intf.voting_session_done),
+		.display_results(intf.display_results),
+		.display_winner(intf.display_winner),
+		.candidate_name(intf.candidate_name),
+		.invalid_results(intf.invalid_results),
+		.results(intf.results),
+		.voting_in_progress(intf.voting_in_progress),
+		.voting_done(intf.voting_done)
   );
 
   bind intf evm_assertion ASSERT(.*);
                   
   initial begin
     rst = 0;
-    @(posedge clk) rst = 1;
-
+		repeat(2) @(posedge clk);
+		rst = 1;
   end
+
   initial begin
-    uvm_config_db#(virtual evm_interface)::set(null, "*", "apb_intf", intf);
-    run_test("wr_test");
+    uvm_config_db#(virtual evm_interface)::set(null, "*", "vif", intf);
+    run_test();
   end
 
 endmodule
