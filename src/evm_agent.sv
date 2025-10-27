652d04f62d2a604af104bfdd44a95d97725780a4
Raj-HR-21 <rajendrahr21@gmail.com>
2025-10-27 16:45:47 +0530

    renamed driver

--- src/evm_agent.sv
+++ src/evm_agent.sv
@@ -4,7 +4,7 @@ class evm_agent_active extends uvm_agent;
 
 	evm_driver drv_h;
 	evm_sequencer sqr_h;
-	evm_monitor_active act_mon_h;
+	evm_monitor_act act_mon_h;
 
 	function new(string name = "evm_agent_active", uvm_component parent = null);
 		super.new(name, parent);
@@ -16,7 +16,7 @@ class evm_agent_active extends uvm_agent;
 		if(is_active == UVM_ACTIVE) begin
 			drv_h = evm_driver::type_id::create("drv_h", this);
 			sqr_h = evm_sequencer::type_id::create("sqr_h", this);
-			act_mon_h = evm_monitor_active::type_id::create("act_mon_h", this);
+			act_mon_h = evm_monitor_act::type_id::create("act_mon_h", this);
 		end
 
 	endfunction: build_phase
@@ -34,7 +34,7 @@ endclass: evm_agent_active
 class evm_agent_passive extends uvm_agent;
 	`uvm_component_utils(evm_agent_passive)
 
-	evm_monitor_passive pas_mon_h;
+	evm_monitor_pass pas_mon_h;
 
 	function new(string name = "evm_agent_passive", uvm_component parent = null);
 		super.new(name, parent);
@@ -44,7 +44,10 @@ class evm_agent_passive extends uvm_agent;
 		super.build_phase(phase);
 		uvm_config_db#(uvm_active_passive_enum)::get(this,"", "is_active", UVM_PASSIVE);
 		if(is_active == UVM_PASSIVE) begin
-			pas_mon_h = evm_monitor_passive::type_id::create("pas_mon_h", this);
+			pas_mon_h = evm_monitor_pass::type_id::create("pas_mon_h", this);
 		end
 
 	endfunction: build_phase
+
+endclass: evm_agent_passive
+
