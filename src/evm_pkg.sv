5e87607c9527dfbc025a27da0555cb176a3e0e1d
nihal-ns <nihalns2002@gmail.com>
2025-10-27 14:58:43 +0530

    added pkg file

---
+++ src/evm_pkg.sv
@@ -0,0 +1,14 @@
+package evm_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  `include "evm_seq_item.sv"
+  `include "evm_sequence.sv"
+  `include "evm_sequencer.sv"
+  `include "evm_driver.sv"
+  `include "evm_monitor.sv"
+  `include "evm_agent.sv"
+  `include "evm_scoreboard.sv"
+  `include "evm_subscriber.sv"
+  `include "evm_env.sv"
+  `include "test.sv"
+endpackage
