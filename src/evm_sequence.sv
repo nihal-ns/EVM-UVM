eb56eea275fedfa2f7de9bf0a16d75ee78024142
OmkarBhat08 <omkarbhat@feserver.mirafra.com>
2025-10-28 15:39:57 +0530

    Added new sequences and test

--- src/evm_sequence.sv
+++ src/evm_sequence.sv
@@ -144,3 +144,89 @@ class evm_sudden_done_sequence extends uvm_sequence #(evm_seq_item);
 			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} dist {1:=7, 2:=2, 4:=1}; req.voting_session_done == 1; req.display_results == 0; req.display_winner == 0;});
 	endtask: body
 endclass: evm_sudden_done_sequence
+
+
+//-----------------------------------------------------------------------------------------------------------------------------
+// Candidate vote count
+class evm_count_cast_sequence extends uvm_sequence #(evm_seq_item);
+	`uvm_object_utils(evm_count_cast_sequence)
+
+	function new(string name = "evm_count_cast_sequence");
+		super.new(name);
+	endfunction: new
+
+	virtual task body();
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+		repeat(20)
+		begin
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 1; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} inside {1, 2, 4}; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+		end
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 0; req.display_winner == 0;});
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 1; req.display_winner == 0;});
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 2; req.display_winner == 0;});
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 0; req.display_winner == 1;});
+	endtask: body
+endclass: evm_count_cast_sequence
+//-----------------------------------------------------------------------------------------------------------------------------
+// Cast 2 votes
+class evm_double_cast_sequence extends uvm_sequence #(evm_seq_item);
+	`uvm_object_utils(evm_double_cast_sequence)
+
+	function new(string name = "evm_double_cast_sequence");
+		super.new(name);
+	endfunction: new
+
+	virtual task body();
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+		repeat(3)
+		begin
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 1; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} inside {1, 2, 4}; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+		end
+
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 1; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 3; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 0; req.display_winner == 1;});
+	endtask: body
+endclass: evm_double_cast_sequence
+//-----------------------------------------------------------------------------------------------------------------------------
+// Vote before ready
+class evm_before_ready_sequence extends uvm_sequence #(evm_seq_item);
+	`uvm_object_utils(evm_before_ready_sequence)
+
+	function new(string name = "evm_before_ready_sequence");
+		super.new(name);
+	endfunction: new
+
+	virtual task body();
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} inside {1, 2, 4}; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 1; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 0; req.display_winner == 1;});
+	endtask: body
+endclass: evm_before_ready_sequence
+//-----------------------------------------------------------------------------------------------------------------------------
+// Default display check
+class evm_default_result_sequence extends uvm_sequence #(evm_seq_item);
+	`uvm_object_utils(evm_default_result_sequence)
+
+	function new(string name = "evm_default_result_sequence");
+		super.new(name);
+	endfunction: new
+
+	virtual task body();
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+		repeat(20)
+		begin
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 1; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+			`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} inside {1, 2, 4}; req.voting_session_done == 0; req.display_results == 0; req.display_winner == 0;});
+		end
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 3; req.display_winner == 0;});
+		`uvm_do_with(req,{req.switch_on_evm == 1; req.candidate_ready == 0; {req.vote_candidate_3, req.vote_candidate_2, req.vote_candidate_1} == 0; req.voting_session_done == 1; req.display_results == 0; req.display_winner == 1;});
+	endtask: body
+endclass: evm_default_result_sequence
+//-----------------------------------------------------------------------------------------------------------------------------
--- src/test.sv
+++ src/test.sv
@@ -15,7 +15,9 @@ class evm_test extends uvm_test;
 		print();
 	endfunction: end_of_elaboration
 endclass: evm_test
+
 //----------------------------------------------------------------------------------------------------------------------
+
 class c1_win_test extends evm_test;	// Call this
 	`uvm_component_utils(c1_win_test)
 
@@ -27,6 +29,7 @@ class c1_win_test extends evm_test;	// Call this
 		evm_c1_win_sequence seq;
 		super.run_phase(phase);
 		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
 			seq = evm_c1_win_sequence::type_id::create("seq");	
 			seq.start(env.agt_act.sqr_h);
 		phase.drop_objection(this, "Objection Dropped");
@@ -37,7 +40,9 @@ class c1_win_test extends evm_test;	// Call this
 		print();
 	endfunction: end_of_elaboration
 endclass: c1_win_test
+
 //----------------------------------------------------------------------------------------------------------------------
+
 class rand_win_test extends evm_test;
 	`uvm_component_utils(rand_win_test)
 
@@ -49,6 +54,7 @@ class rand_win_test extends evm_test;
 		evm_rand_win_sequence seq;
 		super.run_phase(phase);
 		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
 			seq = evm_rand_win_sequence::type_id::create("seq");	
 			seq.start(env.agt_act.sqr_h);
 		phase.drop_objection(this, "Objection Dropped");
@@ -59,7 +65,9 @@ class rand_win_test extends evm_test;
 		print();
 	endfunction: end_of_elaboration
 endclass: rand_win_test
+
 //----------------------------------------------------------------------------------------------------------------------
+
 class tie_max_test extends evm_test;
 	`uvm_component_utils(tie_max_test)
 
@@ -71,6 +79,7 @@ class tie_max_test extends evm_test;
 		evm_tie_max_sequence seq;
 		super.run_phase(phase);
 		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
 			seq = evm_tie_max_sequence::type_id::create("seq");	
 			seq.start(env.agt_act.sqr_h);
 		phase.drop_objection(this, "Objection Dropped");
@@ -81,7 +90,9 @@ class tie_max_test extends evm_test;
 		print();
 	endfunction: end_of_elaboration
 endclass: tie_max_test
+
 //----------------------------------------------------------------------------------------------------------------------
+
 class tie_min_test extends evm_test;
 	`uvm_component_utils(tie_min_test)
 
@@ -93,6 +104,7 @@ class tie_min_test extends evm_test;
 		evm_tie_min_sequence seq;
 		super.run_phase(phase);
 		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
 			seq = evm_tie_min_sequence::type_id::create("seq");	
 			seq.start(env.agt_act.sqr_h);
 		phase.drop_objection(this, "Objection Dropped");
@@ -103,7 +115,9 @@ class tie_min_test extends evm_test;
 		print();
 	endfunction: end_of_elaboration
 endclass: tie_min_test
+
 //----------------------------------------------------------------------------------------------------------------------
+
 class sudden_off_test extends evm_test;
 	`uvm_component_utils(sudden_off_test)
 
@@ -115,6 +129,7 @@ class sudden_off_test extends evm_test;
 		evm_off_sequence seq;
 		super.run_phase(phase);
 		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
 			seq = evm_off_sequence::type_id::create("seq");	
 			seq.start(env.agt_act.sqr_h);
 		phase.drop_objection(this, "Objection Dropped");
@@ -125,7 +140,9 @@ class sudden_off_test extends evm_test;
 		print();
 	endfunction: end_of_elaboration
 endclass: sudden_off_test
+
 //----------------------------------------------------------------------------------------------------------------------
+
 class sudden_done_test extends evm_test;
 	`uvm_component_utils(sudden_done_test)
 
@@ -137,6 +154,7 @@ class sudden_done_test extends evm_test;
 		evm_sudden_done_sequence seq;
 		super.run_phase(phase);
 		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
 			seq = evm_sudden_done_sequence::type_id::create("seq");	
 			seq.start(env.agt_act.sqr_h);
 		phase.drop_objection(this, "Objection Dropped");
@@ -147,3 +165,103 @@ class sudden_done_test extends evm_test;
 		print();
 	endfunction: end_of_elaboration
 endclass: sudden_done_test
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class vote_count_test extends evm_test;
+	`uvm_component_utils(vote_count_test)
+
+	function new(string name = "vote_count_test", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction: new
+
+	virtual task run_phase(uvm_phase phase);
+		evm_count_cast_sequence seq;
+		super.run_phase(phase);
+		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 30ns);
+			seq = evm_count_cast_sequence::type_id::create("seq");	
+			seq.start(env.agt_act.sqr_h);
+		phase.drop_objection(this, "Objection Dropped");
+		$display("############################################################################################################################");
+	endtask: run_phase
+
+	virtual function void end_of_elaboration();
+		print();
+	endfunction: end_of_elaboration
+endclass: vote_count_test
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class double_vote_test extends evm_test;
+	`uvm_component_utils(double_vote_test)
+
+	function new(string name = "double_vote_test", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction: new
+
+	virtual task run_phase(uvm_phase phase);
+		evm_double_cast_sequence seq;
+		super.run_phase(phase);
+		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
+			seq = evm_double_cast_sequence::type_id::create("seq");	
+			seq.start(env.agt_act.sqr_h);
+		phase.drop_objection(this, "Objection Dropped");
+		$display("############################################################################################################################");
+	endtask: run_phase
+
+	virtual function void end_of_elaboration();
+		print();
+	endfunction: end_of_elaboration
+endclass: double_vote_test
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class vote_before_ready_test extends evm_test;
+	`uvm_component_utils(vote_before_ready_test)
+
+	function new(string name = "vote_before_ready_test", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction: new
+
+	virtual task run_phase(uvm_phase phase);
+		evm_before_ready_sequence seq;
+		super.run_phase(phase);
+		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
+			seq = evm_before_ready_sequence::type_id::create("seq");	
+			seq.start(env.agt_act.sqr_h);
+		phase.drop_objection(this, "Objection Dropped");
+		$display("############################################################################################################################");
+	endtask: run_phase
+
+	virtual function void end_of_elaboration();
+		print();
+	endfunction: end_of_elaboration
+endclass: vote_before_ready_test
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class default_display_test extends evm_test;
+	`uvm_component_utils(default_display_test)
+
+	function new(string name = "default_display_test", uvm_component parent = null);
+		super.new(name, parent);
+	endfunction: new
+
+	virtual task run_phase(uvm_phase phase);
+		evm_default_result_sequence seq;
+		super.run_phase(phase);
+		phase.raise_objection(this, "Objection Raised");
+		phase.phase_done.set_drain_time(this, 20ns);
+			seq = evm_default_result_sequence::type_id::create("seq");	
+			seq.start(env.agt_act.sqr_h);
+		phase.drop_objection(this, "Objection Dropped");
+		$display("############################################################################################################################");
+	endtask: run_phase
+
+	virtual function void end_of_elaboration();
+		print();
+	endfunction: end_of_elaboration
+endclass: default_display_test
