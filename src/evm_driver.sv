24ef2112bf7eb9bde85ce5daaa95cad34351f6ce
Raj-HR-21 <rajendrahr21@gmail.com>
2025-10-28 12:50:11 +0530

    updated driver

--- src/evm_driver.sv
+++ src/evm_driver.sv
@@ -14,7 +14,6 @@ class evm_driver extends uvm_driver#(evm_seq_item);
         endfunction: build_phase
 
         virtual task run_phase(uvm_phase phase);
-                repeat(2)@(vif.drv_cb);
                 forever begin
                         seq_item_port.get_next_item(req);
                         drive_to_dut();
@@ -37,10 +36,7 @@ class evm_driver extends uvm_driver#(evm_seq_item);
                 `uvm_info(" DRV ", $sformatf("switch_on_evm = %0d | candidate_ready = %0d | vote_candidate_1 = %0d | vote_candidate_2 = %0d | vote_candidate_3 = %0d ", req.switch_on_evm, req.candidate_ready, req.vote_candidate_1, req.vote_candidate_2, req.vote_candidate_3), UVM_LOW)
 				`uvm_info(" DRV ", $sformatf("voting_session_done = %0d | display_results = %0d | display_winner = %0d", req.voting_session_done, req.display_results, req.display_winner), UVM_LOW);
 
-                if(req.voting_session_done) begin
-                        $display(" VOTING SESSION IS DONE");
-                end
-                repeat(1)@(vif.drv_cb);
+                @(vif.drv_cb);
         endtask: drive_to_dut
 
 endclass: evm_driver
